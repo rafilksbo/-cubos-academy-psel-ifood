709170efc4983e9bdc4a4520374562a3e0d1829c
Rafa Ferreira <rafinha_bo@hotmail.com>
2021-06-09 14:11:22 -0300

    diminuindo os números em...

---
+++ contagem_regressiva.js
@@ -0,0 +1,9 @@
+function solucao(numero) {
+	for(numero.length; numero >= 0; numero--){
+        console.log(numero)
+    } console.log("KABUM")
+} 
+
+function processData(input) {
+	solucao(parseInt(input, 10));
+} 
